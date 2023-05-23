81a1b0300fb60c5bf6538c751bdfbbac08382c3b
ibanb <ibanb@DESKTOP-QRM1TG7>
2023-05-23 23:26:11 +0200

    Add project files.

---
+++ DartsCounter.Ui.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using DartsCounter.Ui.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace DartsCounter.Ui.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Start()
+        {
+            var dartsModel = new DartsModel(); // Create an instance of DartsModel
+            return View("DartsCounter", dartsModel);
+        }
+
+
+        [HttpPost]
+        public IActionResult Subtraction(DartsModel darts)
+        {
+            if (darts.Score < 181)
+            {
+                darts.StartingValue -= darts.Score;
+            }
+            return View("DartsCounter", darts);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DartsCounter.Ui.Mvc/Models/DartsModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DartsCounter.Ui.Mvc.Models
+{
+    public class DartsModel
+    {
+        // Beginwaarde van een leg = 501 
+        public int StartingValue { get; set; } = 501;
+
+        // Gegooide score van de speler 
+        public int Score { get; set; }
+
+        // Resultaat van de score
+        // public int Result { get; set; }
+    }
+}
+
